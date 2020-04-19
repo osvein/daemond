4c351d4210cbf7e45880737fa09d3dcfe318c50f
osvein <osvein@users.noreply.github.com>
2020-04-19 13:19:43 +0200

    remove int_to_str

--- util.h
+++ util.h
@@ -14,27 +14,3 @@
 	putc('\n', stderr) \
 )
 #define sysfatal(fmt, ...) (syslog(fmt, __VA_ARGS__), exit(1))
-
-/* populates buf from the end
- * returns pointer to start of string inside buf
- * returns NULL if base is invalid or buf is too small
- *
- * an n-byte integer will always fit in n*ceil(8*log(2)/log(base))+1 chars,
- * including null-terminator (e.g. sizeof(val)*3+1 for base=10)
- *
- * an optimizer should be able to inline this and possibly use a shorter type
- * than intmax_t. imaxdiv is avoided because it can prevent this optimization,
- * and internally it usually just does division and modulo seperately anyway.
- */
-static inline char *int_to_str(uintmax_t val, char *buf, size_t buflen, int base) {
-	if (buflen <= 0 || base < 2 || base > 36) return NULL;
-	buf += buflen;
-	*--buf = '\0';
-	while (val > 0) {
-		if (--buflen <= 0) return NULL;
-		int digit = val % base;
-		*--buf = digit > 10 ? digit - 10 + 'a' : digit + '0';
-		val /= base;
-	}
-	return buf;
-}
